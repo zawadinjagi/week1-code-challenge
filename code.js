3cc9f3a08e7d56ac98e49ea2284797278af67f16
zawadinjagi <zawadinjagi619@gmail.com>
2024-03-23 19:38:44 +0300

    adding js file and content for challenge

---
+++ code.js
@@ -0,0 +1,11 @@
+document.getElementById("studentsMarks").addEventListener("click", function() {
+    window.location.href = "student-grade/studentMarks.html";
+});
+
+document.getElementById("speed-detector").addEventListener("click", function() {
+    window.location.href = "speed-detector/speedindex.html";
+});
+
+document.getElementById("net-salary-calculator").addEventListener("click", function() {
+    window.location.href = "net-salary-calculator/netsalary.html";
+});
