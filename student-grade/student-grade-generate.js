19d3223e5ff8606bb5a50e2d78ed072175f4b13e
zawadinjagi <zawadinjagi619@gmail.com>
2024-03-23 19:05:41 +0300

    adding js file

---
+++ student-grade/student-grade-generate.js
@@ -0,0 +1,27 @@
+const calculateButton = document.getElementById("calculateButton");
+
+calculateButton.addEventListener("click", calculateGrade);
+
+function calculateGrade() {
+    const JavaScript = parseFloat(document.getElementById("JavaScript").value);
+    const CSS = parseFloat(document.getElementById("CSS").value);
+    const HTML = parseFloat(document.getElementById("HTML").value);
+
+    const averageMark = (JavaScript + CSS + HTML) / 3;
+
+    let grade;
+    if (averageMark >= 80) {
+        grade = "A";
+    } else if (averageMark >= 60) {
+        grade = "B";
+    } else if (averageMark >= 50) {
+        grade = "C";
+    } else if (averageMark >= 40) {
+        grade = "D";
+    } else {
+        grade = "E";
+    }
+    
+    document.getElementById("averageMark").innerText = "Average Mark: " + averageMark.toFixed(2);
+    document.getElementById("grade").innerText = "Grade: " + grade;
+}
